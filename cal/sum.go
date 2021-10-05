11672b78f6fd45b0a815c956ab53857affd5863c
ililiiilil <gjwnsgod1@naver.com>
2021-10-05 21:08:41 +0900

    go study

---
+++ cal/sum.go
@@ -0,0 +1,5 @@
+package cal
+
+func Sum(a, b int) int {
+	return a + b 
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"cal"
+	"fmt"
+)
+
+func main() {
+	result := cal.Sum(1, 2)
+	fmt.Println(result)
+}
